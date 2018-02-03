d47c44cc0de082f25338e9b09ff3593634e2c497
adeyahya <adeyahyaprasetyo@gmail.com>
2018-02-03 23:14:32 +0700

    create routes

---
+++ routes.js
@@ -0,0 +1,42 @@
+const news = require('./actions/news');
+const fetch = require('node-fetch');
+
+module.exports = [
+  {
+    firstMatch: /(berita|news)/,
+    secondMatch: /((hari ini)|(dino iki)|selanjutnya)/i,
+    action: () => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          responseText = await news.getNews();
+          resolve(responseText);
+        } catch(e) {
+          reject(e);
+        }
+      });
+    }
+  },
+  {
+    firstMatch: /((terima kasih)|makasih|kesuwon|(matur nuwun))/,
+    action: () => {
+      return new Promise((resolve, reject) => {
+        resolve('macama kakak 😬');
+      });
+    }
+  },
+  {
+    firstMatch: /(bpi|bitcoin)/i,
+    secondMatch: /((hari ini)|(dino iki))/i,
+    action: () => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice/idr.json');
+          const data = await response.json();
+          resolve(`Harga bitcoin hari ini ${data.bpi.IDR.rate} IDR kakak ...`);
+        } catch(e) {
+          reject(e);
+        }
+      });
+    }
+  },
+]
