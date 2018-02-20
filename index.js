f5cd436415b9987487aae0a8432610d06d4ca7e3
ade yahya <adeyahyaprasetyo@gmail.com>
2018-02-20 22:03:28 +0700

    fixed citchat delay

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require('path');
 const Telegraf = require('telegraf');
 const routes = require('./routes');
 const utils = require('./utils');
-const {wikipedia} = utils;
-const TurndownService = require('turndown')
-const turndownService = new TurndownService()
-const slicer = require("./actions/slicer");
-const translate = require("google-translate-api");
+const {
+  wikipedia
+} = utils;
+const TurndownService = require('turndown');
+const turndownService = new TurndownService();
+const slicer = require('./actions/slicer');
+const translate = require('google-translate-api');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -62,64 +64,71 @@ bot.command('wiki', async ctx => {
   }
 });
 
-bot.command("builtwith", async (ctx) => {
+bot.command('builtwith', async (ctx) => {
   try {
-      let message = ctx.update.message.text;
-      if (message === "/builtwith") {return false;}
-      message = message.replace("/builtwith ", "");
-      const response = await slicer.scrape(message);
-      ctx.replyWithMarkdown(response)
-      .catch(() => ctx.reply("aku ndak tau kalo itu kak"));
-  } catch (e) {
-      ctx.reply("aku ndak tau kalo itu kak");
+    let message = ctx.update.message.text;
+    if (message === '/builtwith') {
+      return false;
+    }
+    message = message.replace('/builtwith ', '');
+    const response = await slicer.scrape(message);
+    ctx.replyWithMarkdown(response)
+      .catch(() => ctx.reply('aku ndak tau kalo itu kak'));
+  } catch (err) {
+    ctx.reply(err);
   }
 });
 
-bot.command("translate", async (ctx) => {
+bot.command('translate', async (ctx) => {
   let message = ctx.update.message.text;
-  if (message === "translate") {return false;}
-  message = message.replace("/translate ", "");
-  translate(message, {from: "auto", to: "id"}).then((res) => {
+  if (message === 'translate') {
+    return false;
+  }
+  message = message.replace('/translate ', '');
+  translate(message, {
+    from: 'auto',
+    to: 'id'
+  }).then((res) => {
     ctx.reply(res.text);
   }).catch((err) => {
-    ctx.reply("tak tahu aku kak");
+    ctx.reply(err);
   });
 });
 
 let randomReply = true;
 bot.on('text', (ctx) => {
   if (randomReply) {
+    randomReply = false;
     const message = utils.getRandomMessage();
     if (typeof message === 'string') {
       ctx.reply(message);
     }
 
     switch (message.type) {
-      case 'video':
-        ctx.replyWithVideo({
-          source: message.source,
-        });
-        break;
-      case 'photo':
-        ctx.replyWithPhoto({
-          source: message.source,
-        });
-        break;
-      case 'audio':
-        return ctx.replyWithAudio({
-          source: message.source,
-        })
-      case 'markdown':
-        ctx.replyWithMarkdown(message.source);
-        break;
-      default:
-        return;
+    case 'video':
+      ctx.replyWithVideo({
+        source: message.source,
+      });
+      break;
+    case 'photo':
+      ctx.replyWithPhoto({
+        source: message.source,
+      });
+      break;
+    case 'audio':
+      return ctx.replyWithAudio({
+        source: message.source,
+      });
+    case 'markdown':
+      ctx.replyWithMarkdown(message.source);
+      break;
+    default:
+      return;
     }
 
-    randomReply = false;
     setTimeout(() => {
       randomReply = true;
-    }, 30000 * 1);
+    }, 60000 * 10);
   }
 });
 
