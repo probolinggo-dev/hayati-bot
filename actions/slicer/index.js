c6c7f0a6352051661d45129a4b06f83a32dda0ec
ade yahya <adeyahyaprasetyo@gmail.com>
2018-02-20 21:43:32 +0700

    fixed indentation, replaced var with let

--- actions/slicer/index.js
+++ actions/slicer/index.js
@@ -8,50 +8,50 @@ const slicer = require("./slicer");
 
 // text telegram format
 const printData = (data) => {
-    var print = "";
-    data.forEach(function (value) {
-        print += "*" + value.title + "*\n";
-        let items = value.item;
-        items.forEach(function (item) {
-            print += "- _" + item.title + "_\n";
-        });
+  let print = "";
+  data.forEach(function (value) {
+    print += "*" + value.title + "*\n";
+    let items = value.item;
+    items.forEach(function (item) {
+      print += "- _" + item.title + "_\n";
     });
+  });
 
-    return print;
+  return print;
 };
 
 const scrape = async (url) => {
-    return new Promise(
-        (resolve, reject) => {
-            const options = {
-                url : "https://builtwith.com/"+ url,
-                headers : {
-                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36" 
-                }
-            };
+  return new Promise(
+      (resolve, reject) => {
+        const options = {
+          url: "https://builtwith.com/" + url,
+          headers: {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+          }
+        };
 
-            request(options, function(error, response, body){
-                if (!error && response.statusCode === 200) {
-                    try {
-                        let data = slicer.builtweb(body);
-                        resolve(data);
-                    } catch (err) {
-                        reject("Maaf terjadi gangguan");
-                    }
-                } else {
-                    reject("Jaringan bermasalah"); 
-                } 
-            });    
-        } 
+        request(options, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            try {
+              let data = slicer.builtweb(body);
+              resolve(data);
+            } catch (err) {
+              reject("Maaf terjadi gangguan");
+            }
+          } else {
+            reject("Jaringan bermasalah");
+          }
+        });
+      }
     )
     .then((data) => {
-        return printData(data); 
+      return printData(data);
     })
     .catch((reason) => {
-        return reason; 
+      return reason;
     });
 };
 
 module.exports = {
-    scrape
+  scrape
 };
--- actions/slicer/slicer.js
+++ actions/slicer/slicer.js
@@ -1,48 +1,47 @@
 // get necessary data from built with com
 const builtWithSlicer = (contents) => {
-    // sanitize content
-    contents = contents.replace(/(?:\\[rn]|[\r\n]|\\|\_+)+/g, "");
-    let slice = contents.split("<div class=\"container\"><section><div class=\"row\"><div class=\"span8\"><div class=\"titleBox\">");
-    slice = slice[1].split("<div class=\"titleBox noP\"><ul class=\"nav nav-pills\"><li><span>Access more of BuiltWith</span></li></ul>");
-    
-    // get the base content
-    let baseContent = slice[0];
-    let sliceTitleBox = baseContent.split("<div class=\"titleBox\">");
-    
-    // get the necessary data
-    let data = [];
-    for (let body of sliceTitleBox) {
-        let sliceTitle = body.split("<li class=\"active\"><span>");
-        sliceTitle = sliceTitle[1].split("</span></li>");
-        let title = sliceTitle[0];
+  // sanitize content
+  contents = contents.replace(/(?:\\[rn]|[\r\n]|\\|\_+)+/g, "");
+  let slice = contents.split("<div class=\"container\"><section><div class=\"row\"><div class=\"span8\"><div class=\"titleBox\">");
+  slice = slice[1].split("<div class=\"titleBox noP\"><ul class=\"nav nav-pills\"><li><span>Access more of BuiltWith</span></li></ul>");
 
-        let sliceItem = sliceTitle[1];
-        sliceItem = sliceItem.split("<div class=\"techItem\">");
-        sliceItem.splice(0, 1); // remove first element
+  // get the base content
+  let baseContent = slice[0];
+  let sliceTitleBox = baseContent.split("<div class=\"titleBox\">");
 
-        let item = [];
-        for (let bodyItem of sliceItem) {
-            let subSliceItem = bodyItem.split("</a></h3>");
-            let sliceItemTitle = subSliceItem[0];
-            sliceItemTitle = sliceItemTitle.split("//trends.builtwith.com/");
-            sliceItemTitle = sliceItemTitle[2].split("\">");
-            let itemTitle = sliceItemTitle[1];
+  // get the necessary data
+  let data = [];
+  for (let body of sliceTitleBox) {
+    let sliceTitle = body.split("<li class=\"active\"><span>");
+    sliceTitle = sliceTitle[1].split("</span></li>");
+    let title = sliceTitle[0];
 
-            item.push({
-                "title" : itemTitle,
-            });
-        }
-        
-        data.push({
-            title, 
-            item
-        });
+    let sliceItem = sliceTitle[1];
+    sliceItem = sliceItem.split("<div class=\"techItem\">");
+    sliceItem.splice(0, 1); // remove first element
+
+    let item = [];
+    for (let bodyItem of sliceItem) {
+      let subSliceItem = bodyItem.split("</a></h3>");
+      let sliceItemTitle = subSliceItem[0];
+      sliceItemTitle = sliceItemTitle.split("//trends.builtwith.com/");
+      sliceItemTitle = sliceItemTitle[2].split("\">");
+      let itemTitle = sliceItemTitle[1];
+
+      item.push({
+        "title": itemTitle,
+      });
     }
 
-    return data;
+    data.push({
+      title,
+      item
+    });
+  }
+
+  return data;
 };
 
 module.exports = {
-    builtweb: builtWithSlicer
+  builtweb: builtWithSlicer
 };
- 
