e121be98ace9a4977ecd9e0cd4e8d4cd4fa5658d
Uddin Mtm <saifuddinmutammam@gmail.com>
2018-02-24 17:03:59 +0700

    add random user agent and some header attribute

--- actions/slicer/index.js
+++ actions/slicer/index.js
@@ -23,15 +23,22 @@ const printData = (data) => {
 const scrape = async (url) => {
     return new Promise(
         (resolve, reject) => {
+            let headers = ["Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36"];
+
             const options = {
                 url : "https://builtwith.com/"+ url,
                 headers : {
-                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36" 
+                    "User-Agent": headers[Math.floor((Math.random() * 3) + 0)],
+                    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                    "Connection":"keep-alive"
                 }
             };
 
             request(options, function(error, response, body){
-                if (!error && response.statusCode === 200) {
+                if (!error && response.statusCode == 200) {
                     try {
                         let data = slicer.builtweb(body);
                         resolve(data);
@@ -45,7 +52,7 @@ const scrape = async (url) => {
         } 
     )
     .then((data) => {
-        return printData(data); 
+        return printData(data);
     })
     .catch((reason) => {
         return reason; 
